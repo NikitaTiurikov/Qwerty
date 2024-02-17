1c6ab669ab765d6d93abd792918c3002b03e7a06
Bogdan Gladkov <bodyanskiy20@gmail.com>
2024-02-17 17:44:12 +0100

    create list

---
+++ task.py
@@ -0,0 +1,5 @@
+import random
+
+my_list = ['Yellow', 'Pink', 'Green', 'Red', 'Black', 'White']
+
+
--- task1.py
+++
@@ -1,6 +0,0 @@
-import random
-
-my_list = ['Yellow', 'Pink', 'Green', 'Red', 'Black', 'White']
-
-res = ' '.join(random.shuffle[my_list])
-print(res)
